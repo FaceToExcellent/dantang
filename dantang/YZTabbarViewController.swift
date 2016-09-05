35acdf2fa5b96567c7724a59ae1bbddbc7d322af
huamanlou <15810427253@163.com>
2016-09-05 12:36:10 +0800

    233333

---
+++ dantang/YZTabbarViewController.swift
@@ -0,0 +1,27 @@
+//
+//  YZTabbarViewController.swift
+//  dantang
+//
+//  Created by HYZ on 16/9/5.
+//  Copyright © 2016年 HYZ. All rights reserved.
+//
+
+import UIKit
+
+class YZTabbarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+  
+
+}
