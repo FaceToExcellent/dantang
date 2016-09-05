0c54c01f3eba91ac74e455653efa8c8e7dd31bfb
huamanlou <15810427253@163.com>
2016-09-05 12:17:58 +0800

    233

---
+++ dantang/ROOTViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ROOTViewController.swift
+//  单糖仿写
+//
+//  Created by HYZ on 16/9/5.
+//  Copyright © 2016年 HYZ. All rights reserved.
+//
+
+import UIKit
+
+class ROOTViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
