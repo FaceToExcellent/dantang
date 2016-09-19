6fa10b45507d8996e76bc9aa76c137167d7bdb7d
huamanlou <15810427253@163.com>
2016-09-19 10:39:30 +0800

    1

---
+++ dantang/Homemodel.swift
@@ -0,0 +1,34 @@
+//
+//  Homemodel.swift
+//  dantang
+//
+//  Created by HYZ on 16/9/18.
+//  Copyright © 2016年 HYZ. All rights reserved.
+//
+/*  {
+ "content_url" = "http://dantang.liwushuo.com/posts/2792/content";
+ "cover_image_url" = "http://7fvaoh.com3.z0.glb.qiniucdn.com/image/151027/pktvp21vq.jpg-w720";
+ "created_at" = 1451606400;
+ "editor_id" = "<null>";
+ id = 2792;
+ labels =                 (
+ );
+ liked = 0;
+ "likes_count" = 534;
+ "published_at" = 1451606400;
+ "share_msg" = "";
+ "short_title" = "\U6700\U7f8e\U88c5\U9970\U706f";
+ status = 0;
+ template = "";
+ title = "\U88c5\U9970\U706f\U767b\U573a\Uff0c\U6c14\U6c1bturn up\Uff01";
+ type = post;
+ "updated_at" = 1445937578;
+ url = "http://dantang.liwushuo.com/posts/2792";
+ },
+*/
+
+import UIKit
+
+class Homemodel: NSObject {
+
+}
