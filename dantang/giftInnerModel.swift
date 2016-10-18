a0717ee5f617888e17bf43eed15fcce60ad2ab77
huamanlou <15810427253@163.com>
2016-10-18 14:33:22 +0800

    1

---
+++ dantang/giftInnerModel.swift
@@ -0,0 +1,37 @@
+//
+//  giftInnerModel.swift
+//  dantang
+//
+//  Created by HYZ on 16/10/18.
+//  Copyright © 2016年 HYZ. All rights reserved.
+//
+
+import UIKit
+
+class giftInnerModel: NSObject {
+    var brand_id :String?
+    var brand_order :String?
+    var category_id :Int?
+    var cover_image_url : Int?
+    var created_at :Int?
+    var mydescription :String?
+    var published_at :Int?
+    var editor_id :String?
+    var favorites_count :Int?
+    var id_ :Int?
+    var image_urls :NSArray?
+    var is_favorite :Int?
+    
+    var name :String?
+    var post_ids :NSArray?
+    var purchase_id :Int?
+    var purchase_status :NSArray?
+    var purchase_type :Int?
+    
+    var purchase_url :String?
+    var subcategory_id :NSArray?
+    var updated_at :Int?
+    var url :NSArray?
+
+    
+}
