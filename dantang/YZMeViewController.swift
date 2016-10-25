dd4d20f2f30a0a5df2ee8fc1c83ccb18f8d7e82d
huamanlou <15810427253@163.com>
2016-10-25 10:10:14 +0800

    233不想写

--- dantang/YZMeViewController.swift
+++ dantang/YZMeViewController.swift
@@ -8,15 +8,41 @@
 
 import UIKit
 
-class YZMeViewController: UIViewController {
-
+class YZMeViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    let tableView=UITableView()
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.isNavigationBarHidden = true
         self.view.backgroundColor = UIColor.white
         // Do any additional setup after loading the view.
+        
+        makeUI()
     }
-
+      func makeUI(){
+        
+        tableView.frame = CGRect.init(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorStyle = UITableViewCellSeparatorStyle.none
+        self.view.addSubview(tableView)
+        tableView.register(UITableViewCell().classForCoder, forCellReuseIdentifier: "me")
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell  = UITableViewCell()
+        cell = tableView.dequeueReusableCell(withIdentifier: "me", for: indexPath)
+    return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
---
+++ dantang/myheadView.swift
@@ -0,0 +1,23 @@
+//
+//  myheadView.swift
+//  dantang
+//
+//  Created by HYZ on 16/10/25.
+//  Copyright © 2016年 HYZ. All rights reserved.
+//
+
+import UIKit
+
+class myheadView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
